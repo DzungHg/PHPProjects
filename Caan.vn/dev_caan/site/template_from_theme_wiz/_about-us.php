4a2030f3ca1801ce59613eb705c8356221a98d9d
DzungHg <hg.dzung@gmail.com>
2019-04-06 06:37:24 +0700

    a

---
+++ Caan.vn/dev_caan/site/template_from_theme_wiz/_about-us.php
@@ -0,0 +1,40 @@
+<!-- =======================
+	about us  -->
+	<section>
+		<div class="container">
+			<div class="row justify-content-between align-items-center">
+				<!-- left -->
+				<div class="col-md-6">
+					<div class="row mt-4 mt-md-0">
+						<div class="col-5 offset-1 px-2 mb-3 align-self-end">
+							<img class="border-radius-3 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s" src="assets/images/service/01.jpg" alt="">
+						</div>
+						<div class="col-6 px-2 mb-3">
+							<img class="border-radius-3 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.6s" src="assets/images/service/02.jpg" alt="">
+						</div>
+						<div class="col-7 px-2 mb-3">
+							<div class="border-radius-3 wow fadeInLeft bg-grad p-2 p-sm-3 p-lg-4 p-xl-5 all-text-white" data-wow-duration="0.8s" data-wow-delay="0s">
+								<span>Our goal:</span>
+								<h3 class="font-weight-bold">"If we build it, they will come"</h3>
+							</div>
+						</div>
+						<div class="col-5 align-self-start pl-2 mb-3">
+							<img class="border-radius-3 wow fadeInDown" data-wow-duration="0.8s" data-wow-delay="0.2s" src="assets/images/service/03.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<!-- right -->
+				<div class="col-md-6 pl-lg-5">
+					<h2 class="h1">Get it done with the Wizixo HTML5 template </h2>
+					<p class="lead">Partnering with 500+ Fortune companies and mid-sized firms across enterprises, uniquely customized and <abbr title="title">scalable workforce solutions.</abbr></p>
+					<p>There is nothing that can stop you from achieving what you want, except yourself. If you devote yourself to it you will achieve your goal.</p>
+					<div>
+						<a href="#" class="btn btn-dark">Contact us</a>
+						<a href="#" class="btn btn-link">View Portfolio</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- =======================
+	about us  -->
---
+++ Caan.vn/dev_caan/site/template_from_theme_wiz/_init.php
@@ -0,0 +1,4 @@
+<?php
+$headline = $page->get("headline|title");
+$bodycopy = $page->body;
+$actiontext = $page->actionbox;
