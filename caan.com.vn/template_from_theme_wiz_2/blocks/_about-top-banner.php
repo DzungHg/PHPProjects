fa62f60d6c21e477e935459b5de334032d29679a
DzungHg <hg.dzung@gmail.com>
2019-04-10 07:03:41 +0700

    x

--- caan.com.vn/template_from_theme_wiz_2/blocks/_about-top-banner.php
+++ caan.com.vn/template_from_theme_wiz_2/blocks/_about-top-banner.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row all-text-white">
 			<div class="col-md-12 align-self-center">
-				<h1 class="font-weight-bold display-4 display-md-1 mb-2 mb-md-n4 mt-9"><?php $page->title; ?></h1>
+				<h1 class="font-weight-bold display-4 display-md-1 mb-2 mb-md-n4 mt-9"><?php echo $page->title; ?></h1>
 			</div>
 		</div>
 	</div>
