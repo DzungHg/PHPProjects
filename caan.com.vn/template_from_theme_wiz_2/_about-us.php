698a8aca35cb867408c5f44fd50fde8c60a1f534
DzungHg <hg.dzung@gmail.com>
2019-04-13 06:11:35 +0700

    t

--- caan.com.vn/template_from_theme_wiz_2/_about-us.php
+++ caan.com.vn/template_from_theme_wiz_2/_about-us.php
@@ -7,10 +7,10 @@
 			<div class="col-md-6">
 				<div class="row mt-4 mt-md-0">
 					<div class="col-5 offset-1 px-2 mb-3 align-self-end">
-						<img class="border-radius-3 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s" src="assets/images/service/01.jpg" alt="">
+						<img class="border-radius-3 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s" src="<?php echo $config->urls->templates; ?>assets/images/service/01.jpg" alt="">
 					</div>
 					<div class="col-6 px-2 mb-3">
-						<img class="border-radius-3 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.6s" src="assets/images/service/02.jpg" alt="">
+						<img class="border-radius-3 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.6s" src="<?php echo $config->urls->templates; ?>assets/images/service/02.jpg" alt="">
 					</div>
 					<div class="col-7 px-2 mb-3">
 						<div class="border-radius-3 wow fadeInLeft bg-grad p-2 p-sm-3 p-lg-4 p-xl-5 all-text-white" data-wow-duration="0.8s" data-wow-delay="0s">
@@ -19,7 +19,7 @@
 						</div>
 					</div>
 					<div class="col-5 align-self-start pl-2 mb-3">
-						<img class="border-radius-3 wow fadeInDown" data-wow-duration="0.8s" data-wow-delay="0.2s" src="assets/images/service/03.jpg" alt="">
+						<img class="border-radius-3 wow fadeInDown" data-wow-duration="0.8s" data-wow-delay="0.2s" src="<?php echo $config->urls->templates; ?>assets/images/service/03.jpg" alt="">
 					</div>
 				</div>
 			</div>
@@ -27,7 +27,7 @@
 			<div class="col-md-6 pl-lg-5">
 				<h2 class="h1"><?php echo $page->home_about_line02 ?> </h2>
 				<p class="lead"><?php echo $page->home_about_line03 ?></abbr></p>
-				<p><?php echo $page->home_about_line04?></p>
+				<p><?php echo $page->home_about_line04 ?></p>
 				<div>
 					<a href="\lien-he" class="btn btn-dark">Liên Hệ</a>
 					<a href="#" class="btn btn-link">Xem Hồ Sơ Năng Lực</a>
