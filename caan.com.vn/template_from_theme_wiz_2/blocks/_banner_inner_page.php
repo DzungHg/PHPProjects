5b9a0508e632250add78bbaa33ce78aed59e7bdc
DzungHg <hg.dzung@gmail.com>
2019-04-13 16:12:59 +0700

    tiep trang lanh vuc

---
+++ caan.com.vn/template_from_theme_wiz_2/blocks/_banner_inner_page.php
@@ -0,0 +1,19 @@
+<!-- =======================
+	Banner innerpage -->
+	<div class="innerpage-banner center bg-overlay-dark-7 py-7" style="background:url(<?php echo $config->urls->templates; ?>assets/images/bg/04.jpg) no-repeat; background-size:cover; background-position: center center;">
+		<div class="container">
+			<div class="row all-text-white">
+				<div class="col-md-12 align-self-center">
+					<h1 class="innerpage-title"><?php echo $page->title; ?></h1>
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item active"><a href="\"><i class="ti-home"></i> Trang Chủ</a></li>
+							<li class="breadcrumb-item">Chi Tiết</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- =======================
+	Banner innerpage -->
---
+++ caan.com.vn/template_from_theme_wiz_2/in-an-theo-nhu-cau.php
@@ -0,0 +1,8 @@
+<div id="page-content" pw-replace>
+   <?php
+   include('./blocks/_banner_inner_page.php');
+   ?>
+   <?php
+   include('./blocks/_in-an-theo-nhu-cau-body.php');
+   ?>
+</div>
