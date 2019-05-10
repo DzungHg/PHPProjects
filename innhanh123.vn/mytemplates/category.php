d70339ba14f034c692ed7b0f9e8eb396d5620772
DzungHg <hg.dzung@gmail.com>
2019-05-10 07:01:20 +0700

    tiep in nhanh

--- innhanh123.vn/mytemplates/category.php
+++ innhanh123.vn/mytemplates/category.php
@@ -3,14 +3,14 @@
 <div id='content'>
 	<?php
 	echo ukHeading1(page()->title, 'divider');
-	$posts = pages()->get('/blog/')->children("categories=$page, limit=10");
-	echo ukBlogPosts($posts); 
+	$posts = pages()->get('/blog/blogs/')->children("categories=$page, limit=10");
+	echo ukBlogPosts($posts);
 	?>
 </div>
 
 <aside id='sidebar'>
 	<?php
 	$categories = page()->parent->children();
-	echo ukNav($categories); 
-	?>		
+	echo ukNav($categories);
+	?>
 </aside>
