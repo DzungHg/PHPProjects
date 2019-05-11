c71c697a7d1a6e2f2045b7910489f7d001bb6c58
DzungHg <hg.dzung@gmail.com>
2019-05-11 16:13:16 +0700

    kti.com
    Làm lại trang này

---
+++ kti.com/config.php
@@ -0,0 +1,103 @@
+<?php namespace ProcessWire;
+
+/**
+ * ProcessWire Configuration File
+ *
+ * Site-specific configuration for ProcessWire
+ *
+ * Please see the file /wire/config.php which contains all configuration options you may
+ * specify here. Simply copy any of the configuration options from that file and paste
+ * them into this file in order to modify them.
+ * 
+ * SECURITY NOTICE
+ * In non-dedicated environments, you should lock down the permissions of this file so
+ * that it cannot be seen by other users on the system. For more information, please
+ * see the config.php section at: https://processwire.com/docs/security/file-permissions/
+ * 
+ * This file is licensed under the MIT license
+ * https://processwire.com/about/license/mit/
+ *
+ * ProcessWire 3.x, Copyright 2016 by Ryan Cramer
+ * https://processwire.com
+ *
+ */
+
+if(!defined("PROCESSWIRE")) die();
+
+/*** SITE CONFIG *************************************************************************/
+
+/** @var Config $config */
+
+/**
+ * Enable debug mode?
+ *
+ * Debug mode causes additional info to appear for use during dev and debugging.
+ * This is almost always recommended for sites in development. However, you should
+ * always have this disabled for live/production sites.
+ *
+ * @var bool
+ *
+ */
+$config->debug = true;
+
+$config->prependTemplateFile = '_init.php';
+$config->appendTemplateFile = '_main.php';
+$config->useMarkupRegions = true;
+$config->useFunctionsAPI = true; 
+
+/*** INSTALLER CONFIG ********************************************************************/
+
+
+/**
+ * Installer: Database Configuration
+ * 
+ */
+$config->dbHost = 'localhost';
+$config->dbName = 'adminkti_db2';
+$config->dbUser = 'adminkti_db';
+$config->dbPass = 'kti@1234';
+$config->dbPort = '3306';
+
+/**
+ * Installer: User Authentication Salt 
+ * 
+ * Must be retained if you migrate your site from one server to another
+ * 
+ */
+$config->userAuthSalt = '94e9c269f081d598139204d8ae481d07'; 
+
+/**
+ * Installer: File Permission Configuration
+ * 
+ */
+$config->chmodDir = '0755'; // permission for directories created by ProcessWire
+$config->chmodFile = '0644'; // permission for files created by ProcessWire 
+
+/**
+ * Installer: Time zone setting
+ * 
+ */
+$config->timezone = 'Asia/Ho_Chi_Minh';
+setlocale(LC_ALL,'en_US.UTF-8');
+/**
+ * Installer: Admin theme
+ * 
+ */
+$config->defaultAdminTheme = 'AdminThemeUikit';
+
+/**
+ * Installer: Unix timestamp of date/time installed
+ * 
+ * This is used to detect which when certain behaviors must be backwards compatible.
+ * Please leave this value as-is.
+ * 
+ */
+$config->installed = 1557560992;
+
+
+/**
+ * Installer: HTTP Hosts Whitelist
+ * 
+ */
+$config->httpHosts = array('dev.kienthucinan.com');
+
