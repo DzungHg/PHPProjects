ca86be8c62f4225b09955baaee1ee1bf78812435
DzungHg <hg.dzung@gmail.com>
2019-05-14 06:30:44 +0700

    caan tiep tuc

--- caan.com.vn/template_from_theme_wiz_2/basic-page.php
+++ caan.com.vn/template_from_theme_wiz_2/basic-page.php
@@ -2,17 +2,34 @@
    <?php
    include('./blocks/_banner_inner_page.php');
    ?>
-   blog -->
+   <!--blog -->
    <section class="blog-page">
       <div class="container">
          <div class="row">
             <!-- blog start -->
-            <div class="col-md-8 mx-auto">
+            <div class="col-md-9 order-first">
                <!-- Post item  with image-->
                <?php echo $page->body ?>
                <!-- End Post item  with image-->
             </div>
+            <aside class="col-md-3 sidebar order-last">
+               <!-- Text Widget -->
+               <?php
+               if ($page->parent) { ?>
+                  <div class="widget">
+                     <h5 class="widget-title">Trang cha</h5>
+                     <div class="details">
+                        <a href="<?php echo $page->parent->url; ?>"><?php echo $page->parent->title; ?></a>
+                     </div>
+                  </div>
+               <?php
+            }
+            ?>
+               <!-- side bar của page này -->
+               <div class="widget">
+                  <?php echo $page->sidebar ?>
+               </div>
+            </aside>
          </div>
-      </div>
    </section>
 </div>
--- caan.com.vn/template_from_theme_wiz_2/blocks/_lanh-vuc-side-bar.php
+++ caan.com.vn/template_from_theme_wiz_2/blocks/_lanh-vuc-side-bar.php
@@ -5,40 +5,46 @@
 		<h5 class="widget-title"><?php echo $page->title ?></h5>
 		<p class="mb-0"><?php echo $page->summary ?></p>
 	</div>
+	<!--các trang con-->
 
-	<!-- Newsletter Widget -->
-	<div class="widget widget-newsletter">
-		<h5 class="widget-title">Tôi nhận tin</h5>
-		<p class="mb-1">Subscribe to our newsletter to receive exclusive offers.</p>
-		<form>
-			<div class="input-group">
-				<input type="email" class="form-control" placeholder="Enter your Email...">
-				<span class="input-group-btn">
-					<button type="submit" class="btn btn-grad mb-0"><i class="fa fa-paper-plane m-0"></i></button>
-				</span>
-			</div>
-		</form>
-	</div>
+	<?php
+	if ($page->numChildren > 0) { ?>
+		<div class="widget widget-post">
+			<h5 class="widget-title">Trang con</h5>
+			<?php
+			foreach ($page->children as $item) { ?>
+				<div class="widget-post clearfix">
+					<div class="details">
+						<a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+					</div>
+				</div>
 
-	<!-- Posts Widget -->
+			<?php
+		};
+		echo "</div>";
+	}
+	?>
 
 
-	<!-- Tag Widget -->
-	<div class="widget">
-		<h5 class="widget-title">Tags</h5>
-		<div class="tags">
-			<a href="#">studio</a>
-			<a href="#">events</a>
-			<a href="#">WordPress</a>
-			<a href="#">gadgets</a>
-			<a href="#">office</a>
-			<a href="#">magazine</a>
-			<a href="#">freebies</a>
-			<a href="#">photograpy</a>
-			<a href="#">updates</a>
-			<a href="#">creative</a>
-			<a href="#">travel</a>
+
+		<!-- Newsletter Widget -->
+		<div class="widget widget-newsletter">
+			<h5 class="widget-title">Tôi nhận tin</h5>
+			<p class="mb-1">Subscribe to our newsletter to receive exclusive offers.</p>
+			<form>
+				<div class="input-group">
+					<input type="email" class="form-control" placeholder="Enter your Email...">
+					<span class="input-group-btn">
+						<button type="submit" class="btn btn-grad mb-0"><i class="fa fa-paper-plane m-0"></i></button>
+					</span>
+				</div>
+			</form>
 		</div>
-	</div>
+
+		<!-- Tag Widget -->
+		<div class="widget">
+			<?php echo $page->sidebar; ?>
+		</div>
+
 </aside>
 <!-- sidebar end -->
