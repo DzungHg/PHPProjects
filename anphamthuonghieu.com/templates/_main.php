2d9761866d1f7445c0bcedae3857bf4b7eb1b22a
DzungHg <hg.dzung@gmail.com>
2019-09-17 06:21:05 +0700

    cơ bản home tmp, base page

--- anphamthuonghieu.com/templates/_main.php
+++ anphamthuonghieu.com/templates/_main.php
@@ -1,4 +1,6 @@
-<?php namespace ProcessWire;
+<?php
+
+namespace ProcessWire;
 
 /**
  * _main.php
@@ -29,85 +31,84 @@
  * See the README.txt file for more information. 
  *
  */
-?><!DOCTYPE html>
-<html lang="en">
+?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
 <head>
+
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>styles/style.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="<?php echo $config->urls->templates ?>css/responsive.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<title><?php echo $title; ?></title>
 	<meta name="description" content="<?php echo $page->summary; ?>" />
-	<link href='//fonts.googleapis.com/css?family=Lusitana:400,700|Quattrocento:400,700' rel='stylesheet' type='text/css' />
-	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
+	<!-- Document Title
+	============================================= -->
+	<title>><?php echo $title; ?></title>
+
 </head>
-<body class="<?php if($sidebar) echo "has-sidebar "; ?>">
 
-	<a href="#main" class="visually-hidden element-focusable bypass-to-main">Skip to content</a>
+<body class="stretched">
 
-	<!-- top navigation -->
-	<ul class='topnav' role='navigation'><?php
-		// top navigation consists of homepage and its visible children
-		foreach($homepage->and($homepage->children) as $item) {
-			if($item->id == $page->rootParent->id) {
-				echo "<li class='current' aria-current='true'><span class='visually-hidden'>Current page: </span>";
-			} else {
-				echo "<li>";
-			}
-			echo "<a href='$item->url'>$item->title</a></li>";
-		}
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
 
-		// output an "Edit" link if this page happens to be editable by the current user
-		if($page->editable()) echo "<li class='edit'><a href='$page->editUrl'>Edit</a></li>";
-	?></ul>
-
-	<!-- search form-->
-	<form class='search' action='<?php echo $pages->get('template=search')->url; ?>' method='get'>
-		<label for='search' class='visually-hidden'>Search:</label>
-		<input type='text' name='q' placeholder='Search' id='search' value='<?php echo $sanitizer->entities($input->whitelist('q')); ?>' />
-		<button type='submit' name='submit' class='visually-hidden'>Search</button>
-	</form>
-
-	<!-- breadcrumbs -->
-	<div class='breadcrumbs' role='navigation' aria-label='You are here:'><?php
-		// breadcrumbs are the current page's parents
-		foreach($page->parents() as $item) {
-			echo "<span><a href='$item->url'>$item->title</a></span> "; 
-		}
-		// optionally output the current page as the last item
-		echo "<span>$page->title</span> "; 
-	?></div>
-
-	<div id='main'>
-
-		<!-- main content -->
-		<div id='content'>
-			<h1><?php echo $title; ?></h1>
-			<?php echo $content; ?>
-		</div>
-
-		<!-- sidebar content -->
-		<?php if($sidebar): ?>
-		<aside id='sidebar'>
-			<?php echo $sidebar; ?>
-		</aside>
-		<?php endif; ?>
-
-	</div>
-
-	<!-- footer -->
-	<footer id='footer'>
-		<p>
-		Powered by <a href='http://processwire.com'>ProcessWire CMS</a>  &nbsp; / &nbsp; 
-		<?php 
-		if($user->isLoggedin()) {
-			// if user is logged in, show a logout link
-			echo "<a href='{$config->urls->admin}login/logout/'>Logout ($user->name)</a>";
-		} else {
-			// if user not logged in, show a login link
-			echo "<a href='{$config->urls->admin}'>Admin Login</a>";
-		}
+		<!-- Top Bar
+		============================================= -->
+		<?php
+		include("./blocks/_top-bar.php");
 		?>
-		</p>
-	</footer>
+		<!-- #top-bar end -->
+
+		<!-- Header
+		============================================= -->
+		<?php
+		include("./blocks/_header.php");
+		?>
+		<!-- #header end -->
+
+		<!-- Content - nơi các trang nhúng html của mình vô đây
+		============================================= -->
+		<section id="content">
+
+
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php
+		include("./blocks/_footer.php");
+		?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script src="<?php echo $config->urls->templates ?>js/jquery.js"></script>
+	<script src="<?php echo $config->urls->templates ?>js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script src="<?php echo $config->urls->templates ?>js/functions.js"></script>
 
 </body>
+
 </html>
---
+++ anphamthuonghieu.com/templates/basic-page.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ProcessWire;
+
+// basic-page.php template file 
+// See README.txt for more information
+
+// Xài thay thế thẻ Section id="content" của _main.php để thay thế
+?>
+<section pw-replace="content">
+	<?php
+	$content = $page->body;
+
+	// If the page has children, then render navigation to them under the body.
+	// See the _func.php for the renderNav example function.
+	if ($page->hasChildren) {
+		$content .= renderNav($page->children);
+	}
+
+	// if the rootParent (section) page has more than 1 child, then render 
+	// section navigation in the sidebar
+	if ($page->rootParent->hasChildren > 1) {
+		$sidebar = renderNavTree($page->rootParent, 3) . $page->sidebar;
+	} ?>
+</section>
