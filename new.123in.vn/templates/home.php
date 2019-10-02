897282aa62758a8d8bd948f6d4b7106d1f2a10be
DzungHg <hg.dzung@gmail.com>
2019-10-02 19:59:54 +0700

    123in.vn tiếp
    Đang làm home

--- new.123in.vn/templates/home.php
+++ new.123in.vn/templates/home.php
@@ -38,8 +38,15 @@ namespace ProcessWire;
 
 						<!-- Main heading -->
 						<h3 class="h3 mb-3"><?php echo $page->home_main_heading->heading_text ?></h3>
-						<?php echo $page->body ?>
-
+						
+						<p><?php echo $page->home_main_heading->heading_text_sub01 ?></p>
+						  <p><?php echo $page->home_main_heading->heading_text_sub02 ?></p>
+						  
+						  <!-- Main heading end -->
+						  <hr>
+						  <p>
+						<?php echo $page->home_main_heading->heading_content ?>
+						</p>
 						<!-- CTA -->
 						<a target="_blank" href="<?php echo $page->home_main_heading->cta_link01 ?>" class="btn btn-indigo btn-md"><?php echo $page->home_main_heading->cta_link01_title ?>
 							<i class="fas fa-download ml-1"></i>
@@ -60,78 +67,8 @@ namespace ProcessWire;
 			<hr class="my-5">
 
 			<!--Section: Main features & Quick Start-->
-			<section>
-
-				<h3 class="h3 text-center mb-5">About MDB</h3>
-
-				<!--Grid row-->
-				<div class="row wow fadeIn">
-
-					<!--Grid column-->
-					<div class="col-lg-6 col-md-12 px-4">
-
-						<!--First row-->
-						<div class="row">
-							<div class="col-1 mr-3">
-								<i class="fas fa-code fa-2x indigo-text"></i>
-							</div>
-							<div class="col-10">
-								<h5 class="feature-title">Bootstrap 4</h5>
-								<p class="grey-text">Thanks to MDB you can take advantage of all feature of newest Bootstrap 4.</p>
-							</div>
-						</div>
-						<!--/First row-->
-
-						<div style="height:30px"></div>
-
-						<!--Second row-->
-						<div class="row">
-							<div class="col-1 mr-3">
-								<i class="fas fa-book fa-2x blue-text"></i>
-							</div>
-							<div class="col-10">
-								<h5 class="feature-title">Detailed documentation</h5>
-								<p class="grey-text">We give you detailed user-friendly documentation at your disposal. It will help you
-									to implement your ideas
-									easily.
-								</p>
-							</div>
-						</div>
-						<!--/Second row-->
-
-						<div style="height:30px"></div>
-
-						<!--Third row-->
-						<div class="row">
-							<div class="col-1 mr-3">
-								<i class="fas fa-graduation-cap fa-2x cyan-text"></i>
-							</div>
-							<div class="col-10">
-								<h5 class="feature-title">Lots of tutorials</h5>
-								<p class="grey-text">We care about the development of our users. We have prepared numerous tutorials,
-									which allow you to learn
-									how to use MDB as well as other technologies.</p>
-							</div>
-						</div>
-						<!--/Third row-->
-
-					</div>
-					<!--/Grid column-->
-
-					<!--Grid column-->
-					<div class="col-lg-6 col-md-12">
-
-						<p class="h5 text-center mb-4">Watch our &quot;5 min Quick Start&quot; tutorial</p>
-						<div class="embed-responsive embed-responsive-16by9">
-							<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cXTThxoywNQ" allowfullscreen></iframe>
-						</div>
-					</div>
-					<!--/Grid column-->
-
-				</div>
-				<!--/Grid row-->
-
-			</section>
+			
+			<?php include("./blocks/_home-main-features.php") ?>
 			<!--Section: Main features & Quick Start-->
 
 			<hr class="my-5">
